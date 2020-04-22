a9bdfb3ec5f904cf0671cbf0276fca83b443cb7c
Kelvin Morrison <kelvinninja1@gmail.com>
2020-04-22 22:40:24 +0000

    Added Contents For Diabetis

--- contents/diabetis/diabetisOverview.php
+++ contents/diabetis/diabetisOverview.php
@@ -1,11 +1,12 @@
-<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+<p>This page summarizes all the diets, exercises and tips for all diabetes patients.
+it also contains frequently asked questions (FAQs) about diabetes.</p>
 <div class="row mt-5 pt-2">
   <div class="col-lg-6">
     <div class="services-2 d-flex">
       <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-first-aid-kit"></span></div>
       <div class="text">
         <h3>Diets</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        <p>This page enlists diets recommended by professional dieticians for the average Ghanaian diabetic.</p>
       </div>
     </div>
   </div>
@@ -14,7 +15,7 @@
       <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-dropper"></span></div>
       <div class="text">
         <h3>Exercises</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        <p>This page enlists exercises recommended by certified and approved dieticians/fitness professionals for the average Ghanaian diabetic.</p>
       </div>
     </div>
   </div>
@@ -23,7 +24,7 @@
       <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-experiment-results"></span></div>
       <div class="text">
         <h3>Tips</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        <p>This page enlists general tips and advices for the general Ghanaian diabetic.</p>
       </div>
     </div>
   </div>
@@ -32,7 +33,7 @@
       <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-heart-rate"></span></div>
       <div class="text">
         <h3>FAQ</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        <p>This page enlists frequently asked questions about diabetics.</p>
       </div>
     </div>
   </div>
--- contents/diabetis/diabetisTips.php
+++ contents/diabetis/diabetisTips.php
@@ -1,38 +1,43 @@
-<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+<p>Tips For Diabetic Patients.</p>
 <div class="row mt-5 pt-2">
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-first-aid-kit"></span></div>
-      <div class="text">
-        <h3>Primary Care</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-dropper"></span></div>
-      <div class="text">
-        <h3>Lab Test</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-lg-6">
+  <div class="col-lg-12">
     <div class="services-2 d-flex">
       <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-experiment-results"></span></div>
       <div class="text">
-        <h3>Symptom Check</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-heart-rate"></span></div>
-      <div class="text">
-        <h3>Heart Rate</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        <h3>Everyday Tips</h3>
+        <!-- <p>Breakfast should be taken before 9am</p> -->
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="services-2 d-flex">
+              <div class="text">
+                <!-- <h3>Beverages/Cereals</h3> -->
+                <!-- <p>Breakfast should be taken before 9am</p> -->
+                <ul>
+                  <li>REMEMBER TO TAKE DRUGS/PRESCIPTIONS ON TIME AND SERIOUSLY</li>
+                  <li>HAVE A CHECKUP AT LEAST ONCE EVERY 3 WEEKS</li>
+                  <li>REPORT EVERY AND ANY COMPLICATIONS TO YOUR DOCTOR</li>
+                  <li>Take exercising seriously </li>
+                  <li>Remember to eat breakfast before 9am</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="services-2 d-flex">
+              <div class="text">
+                <!-- <h3>Porridges</h3> -->
+                <!-- <p>Far far away, behind the word mountains, far from the countries Vokalia.</p> -->
+                <ul>
+                  <li>Avoid fruit juices</li>
+                  <li>Snacks should be taken if one feels hungry after a meal</li>
+                  <li>Remember to eat lunch before 1pm</li>
+                  <li>Drink lots of water </li>
+                  <li>Remember to eat supper before 7pm or don’t sleep immediately after supper, wait at least 2hrs 30mins</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
