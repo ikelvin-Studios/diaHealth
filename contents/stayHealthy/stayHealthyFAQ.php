2855c5d980d325e18937abe6cae4f701d3f9c878
Kelvin Morrison <kelvinninja1@gmail.com>
2020-04-25 18:07:28 +0000

    added FAQ's

--- contents/stayHealthy/stayHealthyFAQ.php
+++ contents/stayHealthy/stayHealthyFAQ.php
@@ -1,39 +1,48 @@
-<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+<p>Frequently Asked Questions about preventing.</p>
 <div class="row mt-5 pt-2">
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-first-aid-kit"></span></div>
-      <div class="text">
-        <h3>Primary Care</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+  <div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Is High Cholesterol Linked to Diabetes?
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        High cholesterol is associated with an elevated risk of cardiovascular disease. That can include coronary heart disease, stroke, and peripheral vascular disease. High cholesterol has also been linked to diabetes and high blood pressure.
       </div>
     </div>
   </div>
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-dropper"></span></div>
-      <div class="text">
-        <h3>Lab Test</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Why does diabetes cause high cholesterol?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        Diabetes tends to lower "good" cholesterol levels and raise triglyceride and "bad" cholesterol levels, which increases the risk for heart disease and stroke. ... Studies show a link between insulin resistance, which is a precursor to type 2 diabetes, and diabetic dyslipidemia, atherosclerosis and blood vessel disease.
       </div>
     </div>
   </div>
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-experiment-results"></span></div>
-      <div class="text">
-        <h3>Symptom Check</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Does stress cause high cholesterol?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        There is compelling evidence that your level of stress can cause an increase in bad cholesterol indirectly. For example, one study found that stress is positively linked to having less healthy dietary habits, a higher body weight, and a less healthy diet, all of which are known risk factors for high cholesterol.
       </div>
     </div>
   </div>
-  <div class="col-lg-6">
-    <div class="services-2 d-flex">
-      <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-heart-rate"></span></div>
-      <div class="text">
-        <h3>Heart Rate</h3>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
-      </div>
-    </div>
-  </div>
+</div>
 </div>
