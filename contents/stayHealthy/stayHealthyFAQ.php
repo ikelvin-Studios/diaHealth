6386aa4ed57deb343a9a5c322d662a822d18222e
Kelvin Morrison <kelvinninja1@gmail.com>
2020-04-26 19:54:28 +0000

    Added Contents to FAQ Sections

--- contents/stayHealthy/stayHealthyFAQ.php
+++ contents/stayHealthy/stayHealthyFAQ.php
@@ -44,5 +44,94 @@
       </div>
     </div>
   </div>
+  <div class="card">
+    <div class="card-header" id="headingFour">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+          What is a normal cholesterol level for a diabetic?
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+      <div class="card-body">
+        Target LDL cholesterol levels for adults with diabetes are <100 mg/dl (2.60 mmol/l); HDL cholesterol levels are >40 mg/dl (1.02 mmol/l); and triglyceride levels are <150 mg/dl (1.7 mmol/l).
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFive">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Which is worse diabetes or cholesterol?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+      <div class="card-body">
+        Cholesterol is important to overall health, but when levels are too high, cholesterol can be harmful by contributing to narrowed or blocked arteries. Unfortunately, people with diabetes are more prone to having unhealthy high cholesterol levels, which contributes to cardiovascular disease (CVD).
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingSix">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          What are the symptoms of high cholesterol in the body?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
+      <div class="card-body">
+        Ask about being tested for high cholesterol. You develop symptoms of heart disease, stroke, or atherosclerosis in other blood vessels, such as left-sided chest pain, pressure, or fullness; dizziness; unsteady gait; slurred speech; or pain in the lower legs
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingSeven">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+          Does walking lower cholesterol?
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
+      <div class="card-body">
+        How Much Exercise Does It Take To Lower Cholesterol? Exactly how much exercise is needed to lower cholesterol has been a matter of some debate. In general, most public health organizations recommend, at a minimum, 30 minutes per day of moderate to vigorous exercise , such as walking, jogging, biking, or gardening.
+
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingEight">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+          What causes diabetes?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
+      <div class="card-body">
+        Diabetes is a chronic condition associated with abnormally high levels of sugar (glucose) in the blood. Insulin produced by the pancreas lowers blood glucose. Absence or insufficient production of insulin, or an inability of the body to properly use insulin causes diabetes.
+
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingNine">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+          Can diabetes be cured?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionExample">
+      <div class="card-body">
+        There is no cure for diabetes, but it can go into remission. People can manage it with medication and lifestyle changes. Type 1 diabetes is an autoimmune disease that develops when the body destroys the cells in the pancreas that produce insulin.
+
+      </div>
+    </div>
+  </div>
 </div>
 </div>
